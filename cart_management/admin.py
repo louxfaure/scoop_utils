5a4aba777217016a077a9ad51905b96e21c55126
louxfaure <louxfaure@gmail.com>
2020-11-23 13:29:20 +0100

    dd

--- cart_management/admin.py
+++ cart_management/admin.py
@@ -41,16 +41,17 @@ class PersonAdmin(admin.ModelAdmin):
 
 @admin.register(Items)
 class ItemsAdmin(admin.ModelAdmin):
-    list_display = ('created','title', 'pickuplocation', 'person', 'appointment')
+    # list_display = ('created','title', 'pickuplocation', 'person', 'appointment')
+    list_display = ('title', 'pickuplocation', 'person', 'appointment')
     ordering = ('pickuplocation', 'person',)
     search_fields = ('person', 'pickuplocation')
-    readonly_fields = ('created','modified')
+    # readonly_fields = ('created','modified')
 
 class ItemsInline(admin.TabularInline):
     model = Items
     fields = ['user_request_id','title','library_name', 'location','call_number','status']
-    readonly_fields = ('status','created','modified')
-
+    # readonly_fields = ('status','created','modified')
+    readonly_fields = ('status')
     def status(self,obj):
         return obj.get_item_status()
 
