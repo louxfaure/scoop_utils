96335a3c6fdae1c0f9c37e967c9d6cf9ddeb1234
louxfaure <louxfaure@gmail.com>
2020-11-30 15:03:10 +0100

    log

--- cart_management/management/commands/check_cart.py
+++ cart_management/management/commands/check_cart.py
@@ -13,16 +13,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         now = datetime.today()
+        print ("{} -- Début du traitement".format(now))
         # print(Items.objects.values('pickuplocation','person' ).filter(appointment__isnull=True).annotate(max_date=Max('created')))       
         for resas_ss_rdv in Items.objects.values('pickuplocation','person' ).filter(appointment__isnull=True, relance_mail=0).annotate(max_date=Max('created')):
-            print(resas_ss_rdv)
+            # print(resas_ss_rdv)
             time_delta = resas_ss_rdv['max_date'] - now
             delta_in_seconds = time_delta.seconds
-            print(delta_in_seconds)
+            # print(delta_in_seconds)
             delta_in_minutes = delta_in_seconds / 60.
-            print(delta_in_minutes)
+            # print(delta_in_minutes)
             if delta_in_minutes > 60 :
-                print("on va faire le job !")
+                # print("on va faire le job !")
             pickup_loc = PickupLocation.objects.get(id_alma=resas_ss_rdv['pickuplocation'])
             lecteur = Person.objects.get(id_alma=resas_ss_rdv['person'])
             url_to_primo = "https://babordplus.hosted.exlibrisgroup.com/primo-explore/account?vid=33PUDB_{}_VU1&section=requests&lang=fr_FR".format(pickup_loc.institution)
@@ -30,7 +31,7 @@ class Command(BaseCommand):
             for resas_lecteurs in Items.objects.filter(appointment__isnull=True,pickuplocation=pickup_loc,person=lecteur):
                 resas_lecteurs.relance_mail =+ 1
                 resas_lecteurs.save()
-                print(resas_lecteurs)
+                print(resas_lecteurs.user_request_id)
 
             send_mail(
                 "{} : Vous devez choisir un créneau de retrait afin de valider votre réservation".format(pickup_loc.name),
