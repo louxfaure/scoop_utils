06a3595c00145b5b733dbec44168b6db54c30ac9
louxfaure <louxfaure@gmail.com>
2021-03-17 11:47:34 +0100

    uodate to 3.6

--- sudoc/services/main.py
+++ sudoc/services/main.py
@@ -30,7 +30,7 @@ def handle_uploaded_file(f,process):
         num_line = 0
         for line in f :
             line = line.rstrip()
-            if (clean_ppn := re.search("(^|\(PPN\))([0-9]{8}[0-9Xx]{1})(;|$)", line.decode())) is None :
+            if (clean_ppn = re.search("(^|\(PPN\))([0-9]{8}[0-9Xx]{1})(;|$)", line.decode())) is None :
                 error = Error(  error_ppn = line.decode(),
                                 error_type = 'PPN_MAL_FORMATE',
                                 error_process = process)
